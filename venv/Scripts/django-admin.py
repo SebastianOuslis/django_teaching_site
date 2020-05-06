4f7a001cca57bfd975f9154b5fb05e9be54e5dc2
SebastianOuslis <sebastian.elo.ouslis@gmail.com>
2020-05-06 15:57:50 -0400

    removed vevn
    removed vevn

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sebastian\documents\django_full_tutorial\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
