9d12fe7cb5d5a63a3a704920e0cdaf795fb059ab
SebastianOuslis <sebastian.elo.ouslis@gmail.com>
2020-05-13 19:37:37 -0400

    fixed static root

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'C:/Users/Sebastian/Documents/GitHub/django_teaching_site/django_project/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
