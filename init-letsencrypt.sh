b39fd4b9bd24f948231a50a129c04b71d445fff8
SebastianOuslis <sebastian.elo.ouslis@gmail.com>
2020-05-25 11:55:25 -0400

    updated scripts and servername for nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(nxtklass.com  www.nxtklass.com)
+domains=(nxtklass.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="nxtklass@gmail.com" # Adding a valid address is strongly recommended
